46f0e29d0e4accf8ff5c1625a0aa8ad36facdffe
Jackson Pybus <jacksonpybus@gmail.com>
2022-09-13 15:50:38 -0400

    Added functionality for scaling histograms

--- root2mpl.py
+++ root2mpl.py
@@ -2,6 +2,8 @@ import ROOT
 import numpy as np
 import matplotlib.pyplot as plt
 
+nObj = 0
+
 class File:
     def __init__(self,infile):
         if (isinstance(infile,ROOT.TFile)):
@@ -9,47 +11,85 @@ class File:
         else:
             self.TFile = ROOT.TFile(infile)
 
-    def get(self,name):
+    def get(self,name,**kwargs):
         if (not self.TFile.GetListOfKeys().Contains(name)):
             raise ValueError("File does not contain specified object")
         h = self.TFile.Get(name)
         if (isinstance(h,ROOT.TH2)):
-            return Hist2D(h)
+            return Hist2D(h,**kwargs)
         elif (isinstance(h,ROOT.TH1)):
-            return Hist1D(h)
+            return Hist1D(h,**kwargs)
         else:
             raise ValueError("Object is not a supported type")
 
+    def getNames(self):
+        return [i.GetName() for i in self.TFile.GetListOfKeys()]
+
+    def plotPoints(self,name,rebin=1,scale=1,**kwargs):
+        h = self.get(name,rebin=rebin,scale=scale)
+        if (isinstance(h,Hist1D)):
+            return h.plotPoints(**kwargs)
+        else:
+            raise ValueError("This is not a 1D histogram and cannot be plotted with this method")
+        
+    def plotBand(self,name,rebin=1,scale=1,**kwargs):
+        h = self.get(name,rebin=rebin,scale=scale)
+        if (isinstance(h,Hist1D)):
+            return h.plotBand(**kwargs)
+        else:
+            raise ValueError("This is not a 1D histogram and cannot be plotted with this method")
+        
+    def plotBar(self,name,rebin=1,scale=1,**kwargs):
+        h = self.get(name,rebin=rebin,scale=scale)
+        if (isinstance(h,Hist1D)):
+            return h.plotBar(**kwargs)
+        else:
+            raise ValueError("This is not a 1D histogram and cannot be plotted with this method")
+        
+    def plotHeatmap(self,name,rebinx=1,rebiny=1,**kwargs):
+        h = self.get(name,rebinx=rebinx,rebiny=rebiny)
+        if (isinstance(h,Hist2D)):
+            return h.plotHeatmap(**kwargs)
+        else:
+            raise ValueError("This is not a 2D histogram and cannot be plotted with this method")
+        
 class Hist1D:
-    def __init__(self,hist):
-        self.TH1 = hist
+    def __init__(self,hist,rebin=1,scale=1):
+        global nObj
+        self.TH1 = hist.Clone(str(nObj))
+        nObj = nObj + 1
+        
+        if (rebin != 1):
+            self.TH1.Rebin(rebin)
+            
         g = ROOT.TGraphAsymmErrors(self.TH1)
         self.x = np.array(g.GetX())
-        self.y = np.array(g.GetY())
+        self.y = np.array(g.GetY())*scale
         xerr = []
         yerr = []
         for i in range(g.GetN()):
             xerr.append(g.GetErrorX(i))
             yerr.append(g.GetErrorY(i))
         self.xerr = np.array(xerr)
-        self.yerr = np.array(yerr)
+        self.yerr = np.array(yerr)*scale
 
     def scale(self,factor):
         self.y *= factor
         self.yerr *= factor
 
     def rebin(self,factor):
-        self.TH1 = self.TH1.Rebin(factor)
-        g = ROOT.TGraphAsymmErrors(self.TH1)
-        self.x = np.array(g.GetX())
-        self.y = np.array(g.GetY())
-        xerr = []
-        yerr = []
-        for i in range(g.GetN()):
-            xerr.append(g.GetErrorX(i))
-            yerr.append(g.GetErrorY(i))
-        self.xerr = np.array(xerr)
-        self.yerr = np.array(yerr)
+        if (factor != 1):
+            self.TH1.Rebin(factor)
+            g = ROOT.TGraphAsymmErrors(self.TH1)
+            self.x = np.array(g.GetX())
+            self.y = np.array(g.GetY())
+            xerr = []
+            yerr = []
+            for i in range(g.GetN()):
+                xerr.append(g.GetErrorX(i))
+                yerr.append(g.GetErrorY(i))
+            self.xerr = np.array(xerr)
+            self.yerr = np.array(yerr)
 
     def areaNorm(self,reference):
         factor = np.sum(reference.y)/np.sum(self.y)
@@ -66,51 +106,59 @@ class Hist1D:
                                 alpha=alpha)
         return line, band
 
-class Hist2D:
-    def __init__(self,hist):
-        self.TH2 = hist
+    def plotBar(self,shift=0,**kwargs):
+        bar = plt.bar(self.x+shift, self.y, **kwargs)
+        
+        return bar
 
-        NX = hist.GetNbinsX()
-        NY = hist.GetNbinsY()
+class Hist2D:
+    def __init__(self,hist,rebinx=1,rebiny=1):
+        global nObj
+        self.TH2 = hist.Clone(str(nObj))
+        nObj = nObj + 1
         
-        left = hist.GetXaxis().GetBinLowEdge(1)
-        right = hist.GetXaxis().GetBinUpEdge(NX)
-        bottom = hist.GetYaxis().GetBinLowEdge(1)
-        top = hist.GetYaxis().GetBinUpEdge(NY)
-        self.extent = (left, right, bottom, top)
-        
-        x = []
-        y = []
+        if (rebinx != 1):
+            self.TH2.RebinX(rebinx)
+        if (rebiny != 1):
+            self.TH2.RebinY(rebiny)
+
+        NX = self.TH2.GetNbinsX()
+        NY = self.TH2.GetNbinsY()
+    
+        xedge = []
+        yedge = []
         z = []
-        xerr = []
-        yerr = []
         zerr = []
         
         for j in range(NX):
-            x.append(hist.GetXaxis().GetBinCenter(j+1))
-            xerr.append(hist.GetXaxis().GetBinWidth(j+1)/2.)
+            xedge.append(self.TH2.GetXaxis().GetBinLowEdge(j+1))
+        xedge.append(self.TH2.GetXaxis().GetBinUpEdge(NX))
     
         for i in range(NY):
-            y.append(hist.GetYaxis().GetBinCenter(i+1))
-            yerr.append(hist.GetYaxis().GetBinWidth(i+1)/2.)
+            yedge.append(self.TH2.GetYaxis().GetBinLowEdge(i+1))
             zcol = []
             zerrcol = []
             for j in range(NX):
-                zval = hist.GetBinContent(j+1,i+1)
+                zval = self.TH2.GetBinContent(j+1,i+1)
                 zcol.append(zval)
-                zerrcol.append(hist.GetBinError(j+1,i+1))
+                zerrcol.append(self.TH2.GetBinError(j+1,i+1))
             z.append(zcol)
             zerr.append(zerrcol)
-
-        self.x = np.array(x)
-        self.y = np.array(y)
+        yedge.append(self.TH2.GetYaxis().GetBinUpEdge(NY))
+            
+        self.xedge = np.array(xedge)
+        self.yedge = np.array(yedge)
         self.z = np.array(z)
-        self.xerr = np.array(xerr)
-        self.yerr = np.array(yerr)
         self.zerr = np.array(zerr)
-        
+    
     def plotHeatmap(self,kill_zeros=True,**kwargs):
         z = self.z
         if (kill_zeros):
-            z = np.ma.masked_where(z == 0, z)
-        return plt.imshow(z,extent=self.extent,**kwargs)
+            z[z==0] = np.nan
+        return plt.pcolormesh(self.xedge,self.yedge,self.z,**kwargs)
+
+    def projectionX(self,**kwargs):
+        return Hist1D(self.TH2.ProjectionX(),**kwargs)
+
+    def projectionY(self,**kwargs):
+        return Hist1D(self.TH2.ProjectionY(),**kwargs)
